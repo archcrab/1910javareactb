8a7fe50f85a99c4a586bb26760072d68ccf66db6
archcrab <ianzhengwenn620@163.com>
2021-06-05 05:19:36 +0800

    problem 10

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -1,6 +1,7 @@
 package com.revature.eval.java.core;
 
 import java.time.temporal.Temporal;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -394,8 +395,17 @@ public class EvaluationService {
 	 * @return
 	 */
 	public List<Long> calculatePrimeFactorsOf(long l) {
-		// TODO Write an implementation for this method declaration
-		return null;
+		List<Long> list = new ArrayList<>();
+		 for(Long i = 2L; i< l; i++) {
+	         while(l%i == 0) {
+	            list.add(i);
+	            l = l/i;
+	         }
+	      }
+	      if(l >2) {
+	         list.add(l);
+	      }
+		return list;
 	}
 
 	/**
